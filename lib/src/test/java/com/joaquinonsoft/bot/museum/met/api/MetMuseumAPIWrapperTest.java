228c6d4110113b919dbe4343f5b0b01da9086cad
JoaquinOnSoft <joaquingp@gmail.com>
2022-10-13 18:58:08 +0200

    Fix bug: Invalid MET object id used

--- lib/src/test/java/com/joaquinonsoft/bot/museum/met/api/MetMuseumAPIWrapperTest.java
+++ lib/src/test/java/com/joaquinonsoft/bot/museum/met/api/MetMuseumAPIWrapperTest.java
@@ -1,6 +1,5 @@
 package com.joaquinonsoft.bot.museum.met.api;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
