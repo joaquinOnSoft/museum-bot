870081a75b4d39c862fdf0652466ba39e343e26c
JoaquinOnSoft <joaquingp@gmail.com>
2022-10-13 23:20:51 +0200

    New transformer

--- lib/src/main/java/com/joaquinonsoft/bot/museum/MuseumAssetTransformer.java
+++ lib/src/main/java/com/joaquinonsoft/bot/museum/MuseumAssetTransformer.java
@@ -2,8 +2,12 @@ package com.joaquinonsoft.bot.museum;
 
 import com.joaquinonsoft.bot.museum.met.pojo.MetMuseumObject;
 import com.joaquinonsoft.bot.museum.pojo.MuseumAsset;
+import com.joaquinonsoft.bot.museum.smithsonian.pojo.Name;
+import com.joaquinonsoft.bot.museum.smithsonian.pojo.Row;
 
 public class MuseumAssetTransformer {
+	private static final String LABEL_ARTIST = "Artist";
+
 	public static MuseumAsset toMusseumAsset(MetMuseumObject metObj) {
 		MuseumAsset asset = null;
 		
@@ -19,4 +23,40 @@ public class MuseumAssetTransformer {
 		
 		return asset;
 	}
+	
+	public static MuseumAsset toMusseumAsset(Row smithsonianContent) {
+		MuseumAsset asset = null;
+		
+		if(smithsonianContent != null) {
+			
+			String artistName = "", date = "", imageLink = "";
+			
+			if (smithsonianContent.getContent() != null) {
+				if (smithsonianContent.getContent().getFreetext() != null) {
+
+					if (smithsonianContent.getContent().getFreetext().getName() != null) {
+						for(Name name: smithsonianContent.getContent().getFreetext().getName()) {
+							if(name.getLabel() == LABEL_ARTIST) {
+								artistName = name.getContent()
+							}
+						}
+					}					
+					
+					if (smithsonianContent.getContent().getFreetext().getDate() != null) {
+						date = smithsonianContent.getContent().getFreetext().getDate().get(0).getContent();
+					}
+				}
+				
+			}
+			
+			asset = new MuseumAsset(
+					smithsonianContent.getTitle(),
+					artistName,
+					date,
+					imageLink					
+					);
+		}
+		
+		return asset;
+	}	
 }
