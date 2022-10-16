257470564532061d513385f4cda994e6c12be343
JoaquinOnSoft <joaquingp@gmail.com>
2022-10-16 18:27:31 +0200

    Added license

---
+++ lib/src/main/java/com/joaquinonsoft/util/FileUtil.java
@@ -0,0 +1,144 @@
+/**   
+ *  (C) Copyright 2022 joaquinonsoft.com and others.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *   Contributors:
+ *     Joaquín Garzón - initial implementation
+ *
+ */
+package com.joaquinonsoft.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 
+ * @author Joaqu�n Garz�n
+ * @since 20.2 
+ */
+public class FileUtil {
+	
+	protected static final Logger log = LogManager.getLogger(FileUtil.class);
+	
+	/**
+	 * Get file from classpath, resources folder
+	 * SEE: Java Read a file from resources folder
+	 * https://www.mkyong.com/java/java-read-a-file-from-resources-folder/
+	 * @param fileName
+	 * @return
+	 */
+	public static File getFileFromResources(String fileName) {
+	    System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        URL resource = FileUtil.class.getClassLoader().getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return new File(resource.getFile());
+        }
+
+    }	
+	
+	public static InputStream getStreamFromResources(String fileName) {
+        InputStream resource = FileUtil.class.getClassLoader().getResourceAsStream(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return resource;
+        }
+
+    }		
+	
+	public static boolean isFile(String path) {
+		boolean isFile = false;
+		
+		File f = new File(path);
+		if(f.exists() && !f.isDirectory()) { 
+		    isFile = true;
+		}
+		
+		return isFile;
+	}
+	
+	public static boolean deleteFile(String path) {
+		boolean deleted = false;
+		
+		File f = new File(path);
+		if(f.exists()) { 
+		    deleted = f.delete();
+		}
+		
+		return deleted;
+	
+	}
+	
+	public static Properties loadProperties(String propFileName) {
+		Properties prop = null;
+		
+		log.debug("Properties file name: " + propFileName);
+		InputStream propFile = FileUtil.getStreamFromResources(propFileName); 
+
+		try {
+			prop = new Properties();
+			log.debug("Loading");
+			prop.load(propFile);
+			log.debug("Loaded");
+		} 
+		catch (FileNotFoundException e) {
+			System.err.println("Properties file not found");
+			e.getSuppressed();
+		}
+		catch (IOException e) {
+			System.err.println("Properties file: " + e.getMessage());
+		}
+		
+		return prop;
+	}
+	
+	public static String getRandomFileName(String extension) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");  
+		String strDate = dateFormat.format(GregorianCalendar.getInstance().getTime());
+		
+		int random = new Random().nextInt(10000);  // [0...10000]
+		String strRandom = String.format("%05d", random);
+		
+		String name = strDate + strRandom;
+		name += extension.startsWith(".") ? extension :  "." + extension;
+		
+		return name;
+	}
+	
+	public static File[] filterFilesByExtension(String path, String extension) {
+		File dir = new File(path);
+		File[] files = dir.listFiles(new FilenameFilter() {
+		    public boolean accept(File dir, String name) {
+		        return name.toLowerCase().endsWith(extension);
+		    }
+		});		
+		
+		return files;
+	}
+}
---
+++ lib/src/test/java/com/joaquinonsoft/util/FileUtilTest.java
@@ -0,0 +1,94 @@
+/**
+ *   (C) Copyright 2021 OpenText and others.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *   Contributors:
+ *     Joaquín Garzón - initial implementation
+ *
+ */
+package com.joaquinonsoft.util;
+
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+
+
+public class FileUtilTest{
+	private static final String XML_EXAMPLE_FILE_NAME = "1257656312529186816.xml";
+
+	@Test
+	public void testDeleteFile() {
+		String fileName = "filename.txt";
+		FileWriter myWriter;
+		try {
+			myWriter = new FileWriter(fileName);
+			myWriter.write("Files in Java might be tricky, but it is fun enough!");
+			myWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		}
+
+		assertTrue(FileUtil.isFile(fileName));
+		FileUtil.deleteFile(fileName);
+		assertFalse(FileUtil.isFile(fileName));
+	}
+	
+	@Test
+	public void testGetFileFromResources() {
+		File f = FileUtil.getFileFromResources(XML_EXAMPLE_FILE_NAME);
+		assertNotNull(f);
+		assertTrue(f.exists());
+	}
+	
+	
+	@Test
+	public void testIsFile() {
+		String cwd = null;
+		try {
+			cwd = (new File( "." )).getCanonicalPath();
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+		
+		assertFalse(FileUtil.isFile(cwd));
+	}
+	
+	@Test
+	public void filterFilesByExtension() {
+		
+		URL resource = getClass().getClassLoader().getResource(XML_EXAMPLE_FILE_NAME);
+		if(resource != null) {			
+			File[] files = FileUtil.filterFilesByExtension(resource.getPath().replace(XML_EXAMPLE_FILE_NAME, ""), ".xml");
+			assertNotNull(files);
+			assertEquals(1, files.length);
+			assertEquals(XML_EXAMPLE_FILE_NAME, files[0].getName());
+		}
+		else {
+			fail("Resource `1257656312529186816.xml` not found.");
+		}
+	}
+}
